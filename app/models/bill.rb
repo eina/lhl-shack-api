199b439da3981c1df95cbdbf06eb7d388c4093e2
CLARE NOLAN <clarenolan92@gmail.com>
2020-03-14 21:08:48 +0000

    add belongs to to models

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -1,4 +1,5 @@
 class Bill < ActiveRecord::Base
   has_many :split_bills
   has_many :users, through: :split_bills
+  belongs_to :households
 end
--- app/models/household.rb
+++ app/models/household.rb
@@ -2,4 +2,6 @@ class Household < ActiveRecord::Base
   has_many :bills
   has_many :agreements
   has_many :documents
+  belongs_to :users
+  belongs_to :landlords
 end
