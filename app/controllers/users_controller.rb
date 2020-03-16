dbba346cd5c40e9618a2cb624e6396a7f26033b2
CLARE NOLAN <clarenolan92@gmail.com>
2020-03-16 15:16:47 +0000

    create users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,39 @@
 class UsersController < ApplicationController
+    # GET /landlords
+    def index 
+      @landlords = Landlord.all
+    end
+  
+    def show
+    end
+  
+    def create
+      @luserr = User.new(user_params)
+      if @user.save
+        render :show, status: :created, location: @user
+      else 
+        render json: @user.errors, status: :unprocessable_entity
+      end
+    end
+  
+    def update
+      if @user.update(user_params)
+        render :show, status: :ok, location: @user
+      else 
+        render json: @todo.errors, status: unprocessable_entity
+      end
+    end
+  
+    def destroy
+      @user.destroy
+    end
+  
+    private 
+      def set_users
+        @user = user.find(params[:id])
+      end
+  
+      def user_params
+        params.require(:user).permit(:first_name, :last_name, :phone_number, :email, :password)
+      end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ActiveRecord::Base
   has_many :households
   validates_presence_of :first_name, :last_name, :phone_number, :email, :password
   validates_format_of :phone_number, with: /\A(\d{10}|\(?\d{3}\)?[-. ]\d{3}[-.]\d{4})\z/
-validates_format_of :email, with: /\A[\w]([^@\s,;]+)@(([\w-]+\.)+(com|edu|org|net|gov|mil|biz|info))\z/i
+  validates_format_of :email, with: /\A[\w]([^@\s,;]+)@(([\w-]+\.)+(com|edu|org|net|gov|mil|biz|info))\z/i
 end
