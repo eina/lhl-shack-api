a5b59050d565923488fa5c46820a5e0812087a6d
Eina Onting <einers@outlook.com>
2020-03-16 04:54:57 +0000

    put resource under a scope to namespace

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  resources :landlords, defaults: { format: :json }
+  # put all api routes inside the namespace for "api/x"
+  scope :api do
+    resources :landlords, defaults: { format: :json }
+  end
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
