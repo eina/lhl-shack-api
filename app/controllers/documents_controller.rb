26d322531bd12b31dd8763c1231f28a75488609c
CLARE NOLAN <clarenolan92@gmail.com>
2020-03-16 18:52:30 +0000

    add documents controller

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,2 +1,39 @@
 class DocumentsController < ApplicationController
+  # GET /Documents
+  def index 
+    @documents = Documents.all
+  end
+
+  def show
+  end
+
+  def create
+    @document = Document.new(document_params)
+    if @Document.save
+      render :show, status: :created, location: @document
+    else 
+      render json: @document.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @document.update(document_params)
+      render :show, status: :ok, location: @document
+    else 
+      render json: @document.errors, status: unprocessable_entity
+    end
+  end
+
+  def destroy
+    @document.destroy
+  end
+
+  private 
+    def set_households
+      @document = document.find(params[:id])
+    end
+
+    def household_params
+      params.require(:document).permit(:s3_identifier)
+    end
 end
