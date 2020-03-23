6ac31e287b851549f46946608c15fd3d1b7626c7
Clare <56135752+YungComputer@users.noreply.github.com>
2020-03-22 17:33:33 -0700

    final commit

---
+++ app/controllers/households_controller.rb
@@ -0,0 +1,57 @@
+class HouseholdsController < ApplicationController
+  before_action :set_households, only: [:show, :update, :destroy]
+
+  # GET /households
+  def index 
+    @households = Household.all
+    render json: @households
+  end
+
+
+  # GET /households/1
+  def show
+    # show the user details in the household. Results should be a JSON object which has household
+#     household: {
+#       id: 1
+#       users: [{}, {}]
+#     }
+# landlord with house id = house_id
+# Users in the household_id = household_id
+
+      # render :show, status: :created, location: @household
+
+    render json: @household
+  end
+
+  def create
+    @household = Household.new(household_params)
+    if @household.save
+      render json: @household, status: :created
+      # render :show, status: :created, location: @household
+    else 
+      render json: @household.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @household.update(household_params)
+      render json: @household, status: :ok
+      # render :show, status: :ok, location: @household
+    else 
+      render json: @household.errors, status: unprocessable_entity
+    end
+  end
+
+  def destroy
+    @household.destroy
+  end
+
+  private 
+    def set_households
+      @household = Household.find(params[:id])
+    end
+
+    def household_params
+      params.require(:household).permit(:user_id, :house_id)
+    end
+end
