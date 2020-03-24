bdc8956d512b0c837ebfffe7829ab1d048595dc4
Eina Onting <einers@outlook.com>
2020-03-24 05:02:51 +0000

    delete associated files with Housekeeping

--- app/models/housekeeping.rb
+++
@@ -1,4 +0,0 @@
-class Housekeeping < ActiveRecord::Base
-  belongs_to :house
-  validates_presence_of :household_id, :housekeeping_values
-end
--- test/models/housekeeping_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class HousekeepingTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
