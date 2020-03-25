30ab44b242add3538b85b4510888166fff9563b9
Eina Onting <einers@outlook.com>
2020-03-25 03:48:33 +0000

    add filter scopes

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -11,7 +11,7 @@ class BillsController < ApplicationController
     # raise params.values_at(:household_id, :bill_uuid, :user_id).all?.inspect
     
     if params.values_at(:household_id, :bill_uuid, :user_id).all?(&:present?)      
-      @bills = Bill.filter_user_single_bills(params)
+      @bills = Bill.filter_user_single_bill(params)
     elsif  params.values_at(:household_id, :bill_uuid).all?(&:present?)
       @bills = Bill.filter_bill_by_household_and_bill_uuid(params)
     elsif  params.values_at(:household_id, :user_id).all?(&:present?)
