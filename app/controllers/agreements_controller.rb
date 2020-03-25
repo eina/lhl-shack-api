577ad09c6247e8f1bd7c750ae947cf010fee24bd
Eina Onting <einers@outlook.com>
2020-03-25 20:59:00 +0000

    add timestamps to agreement pdf and return link as json

--- app/controllers/agreements_controller.rb
+++ app/controllers/agreements_controller.rb
@@ -19,25 +19,35 @@ class AgreementsController < ApplicationController
     end
   
     def create
+      is_html_string =  agreement_params[:html_string].blank?      
       @agreement = Agreement.new(agreement_params)
 
       # initialize s3      
-      # s3 = Aws::S3::Resource.new(region:'us-west-2')
-      
       if @agreement.save
-        # generate pdf        
-        # pdf = WickedPdf.new.pdf_from_string('<h1>Hello There!</h1>')
-        # generate a unique file name probably
-        filename = 'test.pdf'
-        
-        #create in stream
-        # send_data pdf, filename: filename
-        
-        # upload to S3
-        # obj = s3.bucket(ENV['S3_BUCKET']).object(filename)
-        # obj.put(body: pdf)                
+        # raise (!is_html_string).inspect
+        # if html string isn't blank
+        if (!is_html_string)
+          s3 = Aws::S3::Resource.new(region:'us-west-2')      
+          
+          # generate pdf          
+          pdf = WickedPdf.new.pdf_from_string(agreement_params[:html_string])
+          timestamp = Time.now.utc.strftime("%Y%m%d%H%M%S")
+          filename = "agreement-#{timestamp}.pdf"
+          link = "#{ENV['CLOUDFRONT_URL']}/#{filename}"                  
 
-        render json: @agreement, status: :created
+          # upload to S3
+          obj = s3.bucket(ENV['S3_BUCKET']).object(filename)
+          obj.put(body: pdf)
+
+          if (obj.exists? == true)
+            render json: { link: link }, status: :created
+          else 
+            render json: @agreement, status: :created
+          end
+        else 
+          render json: @agreement, status: :created
+        end
+                                          
         # render :show, status: :created, location: @agreement
       else 
         render json: @agreement.errors, status: :unprocessable_entity
@@ -59,6 +69,20 @@ class AgreementsController < ApplicationController
     end
   
     private 
+      def generate_pdf_to_s3 
+          s3 = Aws::S3::Resource.new(region:'us-west-2')      
+          
+          # generate pdf          
+          pdf = WickedPdf.new.pdf_from_string(agreement_params[:html_string])
+          timestamp = Time.now.utc.strftime("%Y%m%d%H%M%S")
+          filename = "agreement-#{timestamp}.pdf"
+          link = "#{ENV['CLOUDFRONT_URL']}/#{filename}"                  
+
+          # upload to S3
+          obj = s3.bucket(ENV['S3_BUCKET']).object(filename)
+          obj.put(body: pdf)
+      end
+      
       def set_agreements                        
         house_agreement = Agreement.where(household_id: params[:id])
         if house_agreement.blank?
